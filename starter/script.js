87d18469f0d435b3b01b4fe73936a21ad2a93bc4
Jason Viox <jrv04@hotmail.com>
2022-01-20 19:51:16 -0600

    Finish string/template literal tutorial

--- starter/script.js
+++ starter/script.js
@@ -150,3 +150,12 @@ console.log(jason);
 //Template literals
 const jasonNew = `I'm ${firstName} a ${year - birthYear} year old ${job}!`;
 console.log(jasonNew)
+console.log(`Just a regular string...`);
+
+console.log('String with \n\
+multiple \n\
+lines');
+
+console.log(`String
+multiple
+lines`);
