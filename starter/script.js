a4957d59388b2e73bb4525bca663c0101a242335
Jason Viox <jrv04@hotmail.com>
2022-01-20 19:36:33 -0600

    first commit

---
+++ starter/script.js
@@ -0,0 +1,140 @@
+// let js = 'amazing';
+// console.log(40 + 8+ 23 - 10);
+
+// console.log('Jonas');
+// console.log(23);
+
+// let firstName = 'Matilda';
+// console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
+
+// let jonas_matilda = 'JM';
+// let $function = 27;
+
+// let person = 'jonas';
+// let PI = 3.1415;
+
+// let myFirstJob = 'Coder';
+// let CurrentJob = 'Retailer';
+
+// let job1 = 'programmer';
+// let job2 = 'retailer';
+
+// console.log(myFirstJob);
+
+// let javascriptIsFun = true;
+// console.log(javascriptIsFun);
+
+// console.log(typeof true);
+// console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof 'Jason');
+
+// javascriptIsFun = 'YES!';
+// console.log(typeof javascriptIsFun);
+
+// let year;
+// console.log(year);
+// console.log(typeof year);
+
+// year = 1991;
+// console.log(typeof year);
+// console.log(typeof null);
+
+
+// const country = "United States";
+// const continent = "North America";
+// let population = 320000000;
+// const isIsland = false;
+// const language = "English";
+// console.log(typeof isIsland);
+// console.log(typeof population);
+// console.log(typeof country);
+// console.log(typeof language);
+
+// console.log(language);
+// console.log(country);
+// console.log(continent);
+// console.log(population);
+
+// console.log(population / 2);
+// population++;
+// console.log(population);
+// console.log(population > 6);
+// console.log(population < 33);
+
+// const description = country + ' is in ' + continent + ', and its ' + population + ' million people speak ' + language;
+// console.log(description);
+// let age = 30;
+// age = 31;
+
+// const birthYear = 1991;
+// birthYear = 1992;
+
+// lastName = "Viox";
+// console.log(lastName);
+
+// Math operators
+// const now = 2037;
+// const ageJason = now - 1991;
+// const ageSarah = now - 2018;
+// console.log(ageJason, ageSarah);
+
+// console.log(ageJason * 2, ageJason / 10, 2 ** 3);
+// // 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+
+// const firstName = "Jason";
+// const lastName = "Viox";
+// console.log(firstName + ' ' + lastName);
+
+// // Assignment operators
+// let x = 10 + 5; // 15
+// x += 10; // x = x + 10 = 25
+// x *= 4; // x + x * 4 = 100
+// x++; // x = x + 1 = 101
+// x--;
+// x--;
+// console.log(x);
+
+// // Comparison operators
+// console.log(ageJason > ageSarah); // >, <, >=, <=
+// console.log(ageSarah >= 18);
+
+// const isFullAge = ageSarah >= 18;
+
+// console.log(now - 1991 > now - 2020);
+
+// const now = 2037;
+// const ageJason = now - 1991;
+// const ageSarah = now - 2018;
+
+// console.log(now - 1991 > now - 2020);
+
+// let x, y;
+// x = y = 25-10-5; // x = y = 10, x = 10,
+// console.log(x, y);
+
+// const averageAge = (ageJason + ageSarah) / 2
+// console.log(ageJason, ageSarah, averageAge);
+
+// //Coding Challenge 1
+// const markWeight1 = 78;
+// const markHeight1 = 1.69;
+// const johnWeight1 = 92;
+// const johnHeight1 = 1.95;
+// const markBMI1 = markWeight1 / (markHeight1 * markHeight1);
+// const johnBMI1 = johnWeight1 / (johnHeight1 * johnHeight1);
+// const markHigherBMI1 = markBMI1 > johnBMI1;
+// console.log(markBMI1, johnBMI1, markHigherBMI1);
+
+// const markWeight2 = 95;
+// const markHeight2 = 1.88;
+// const johnWeight2 = 85;
+// const johnHeight2 = 1.76;
+// const markBMI2 = markWeight2 / (markHeight2 * markHeight2);
+// const johnBMI2 = johnWeight2 / (johnHeight2 * johnHeight2);
+// const markHigherBMI2 = markBMI2 > johnBMI2;
+// console.log(markBMI2, johnBMI2, markHigherBMI2);
+// console.log(markBMI1 > johnBMI1);
+// console.log(markBMI2 > johnBMI2);
