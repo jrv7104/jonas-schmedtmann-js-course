e1874b314153f2764cc1f7f124e382f155c88ea0
Jason Viox <jrv04@hotmail.com>
2022-01-31 16:10:12 -0600

    Truthy/falsy values

--- starter/script.js
+++ starter/script.js
@@ -243,3 +243,19 @@ console.log('23' / '2');
 let n = '1' + 1;
 n = n - 1;
 console.log(n)
+
+// 5 falsy values 0, '', undefined, null, NaN
+
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean('Jason'));
+console.log(Boolean({}));
+console.log(Boolean(''));
+
+const money = 100;
+if(money) {
+    console.log("Don't spend it all ;)");
+} else {
+    console.log('You should get a job!');
+}
+
