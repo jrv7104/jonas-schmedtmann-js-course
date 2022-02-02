93dc04628391b3d254ad892a47ba6e039c341ba2
Jason Viox <jrv04@hotmail.com>
2022-02-02 14:38:55 -0600

    Testing prompt

--- starter/script.js
+++ starter/script.js
@@ -272,3 +272,7 @@ const age = '18';
 if(age === 18) console.log('You just became an adult! (strict)');
 
 if(age == 18) console.log('You just became an adult! (loose)' );
+
+const favorite = prompt("What is your favorite number?");
+console.log(favorite);
+console.log(typeof favorite);
