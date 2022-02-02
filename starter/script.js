5c94098f89fce74aecd643a9d2a295ee8a95a6b6
Jason Viox <jrv04@hotmail.com>
2022-02-02 15:04:52 -0600

    if/else and if else statements

--- starter/script.js
+++ starter/script.js
@@ -273,6 +273,17 @@ if(age === 18) console.log('You just became an adult! (strict)');
 
 if(age == 18) console.log('You just became an adult! (loose)' );
 
-const favorite = prompt("What is your favorite number?");
+const favorite = Number(prompt("What is your favorite number?"));
 console.log(favorite);
 console.log(typeof favorite);
+
+if(favorite === 23) {
+    console.log('Cool! 23 is an amazing number!');
+} else if(favorite === 7) {
+    console.log('7 is also a cool number')
+} else if(favorite === 9) {
+    console.log('9 is also a cool number');
+}
+else {
+    console.log('That is a shame');
+}
