61dd91de3be955e63d655aa79446d4829bc89410
Jason Viox <jrv04@hotmail.com>
2022-02-06 19:54:56 -0600

    Set up conditional ternary operator lesson

--- starter/script.js
+++ starter/script.js
@@ -430,24 +430,26 @@
 //     }
 
 //Switch case exercise
-const language = "Chinese";
+// const language = "Chinese";
 
-switch(language) {
-    case "Chinese":
-        console.log('MOST number of native speakers!');
-        break;
-    case "Spanish":
-        console.log('2nd place in number of native speakers.');
-        break;
-    case "English":
-        console.log('3rd place');
-        break;
-    case "Hindi":
-        console.log('Number 4');
-        break;
-    case "Arabic":
-        console.log('5th most spoken language.');
-        break;
-    default:
-        console.log('Great language too :D');
-}
+// switch(language) {
+//     case "Chinese":
+//         console.log('MOST number of native speakers!');
+//         break;
+//     case "Spanish":
+//         console.log('2nd place in number of native speakers.');
+//         break;
+//     case "English":
+//         console.log('3rd place');
+//         break;
+//     case "Hindi":
+//         console.log('Number 4');
+//         break;
+//     case "Arabic":
+//         console.log('5th most spoken language.');
+//         break;
+//     default:
+//         console.log('Great language too :D');
+// }
+
+//Conditional (ternary) operator lesson
