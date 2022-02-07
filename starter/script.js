ab6daf0498f7649042db3c0121250286df8e83fe
Jason Viox <jrv04@hotmail.com>
2022-02-06 19:36:47 -0600

    Days of week planned out

--- starter/script.js
+++ starter/script.js
@@ -401,4 +401,21 @@ switch(day) {
     case 'Monday': // day === 'Monday'
         console.log('Plan course structure');
         console.log('Go to coding meetup');
-}
+        break;
+    case 'Tuesday':
+        console.log('Prepare theory videos');
+        break;
+    case 'Wednesday':
+    case 'Thursday':
+        console.log('Write code examples');
+        break;
+    case 'Friday':
+        console.log('Record videos');
+        break;
+    case 'Saturday':
+    case 'Sunday':
+            console.log('Enjoy the weekend');
+            break;
+    default:
+        console.log('Not a valid day');
+    }
