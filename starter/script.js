3ed936e4ad36fbc2d6503bd4e3a31da07f8d1d4c
Jason Viox <jrv04@hotmail.com>
2022-02-06 19:53:55 -0600

    Switch case exercise complete

--- starter/script.js
+++ starter/script.js
@@ -428,3 +428,26 @@
 //     } else {
 //         console.log("Enjoy the weekend");
 //     }
+
+//Switch case exercise
+const language = "Chinese";
+
+switch(language) {
+    case "Chinese":
+        console.log('MOST number of native speakers!');
+        break;
+    case "Spanish":
+        console.log('2nd place in number of native speakers.');
+        break;
+    case "English":
+        console.log('3rd place');
+        break;
+    case "Hindi":
+        console.log('Number 4');
+        break;
+    case "Arabic":
+        console.log('5th most spoken language.');
+        break;
+    default:
+        console.log('Great language too :D');
+}
