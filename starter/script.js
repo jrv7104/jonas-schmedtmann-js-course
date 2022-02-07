b913d3009d351f2386dab90f07bcfc7e25121b22
Jason Viox <jrv04@hotmail.com>
2022-02-07 09:43:53 -0600

    Finish lesson, start activity

--- starter/script.js
+++ starter/script.js
@@ -453,5 +453,20 @@
 // }
 
 //Conditional (ternary) operator lesson
-const age = 15;
-age >= 18 ? console.log('I like to drink wine.') : console.log('I like to drink water.')
+// const age = 23;
+// age >= 18 ? console.log('I like to drink wine.') : console.log('I like to drink water.')
+
+// const drink = age >= 18 ? 'wine' : 'water';
+// console.log(drink);
+
+// let drink2;
+// if(age >= 18) {
+//     drink2 = 'wine';
+// } else {
+//     drink2 = 'water';
+// }
+// console.log(drink2);
+
+// console.log(`I like to drink ${age >= 18 ? 'wine' : 'water'}`);
+
+//conditional (ternary) operator activity
